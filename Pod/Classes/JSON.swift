9086c3131ed659dfc04d83afcadd744101c9556b
Marcus Kida <marcus@kida.io>
2016-03-18 19:44:49 +1100

    Serializer works for simple objects

--- Pod/Classes/JSON.swift
+++ Pod/Classes/JSON.swift
@@ -8,36 +8,53 @@
 
 import Foundation
 
+typealias Mapped = [String: AnyObject]
+
 struct JSON {
     let game: Mapable
-    
+}
+
+//MARK: Serialization
+extension JSON {
     func serialize() -> String? {
-        guard let s = serialize(game, mapped: [String: Mapable]()) else {
-            print("Serializing went wrong")
-            return nil
-        }
-        guard let si = serialize(s) else { return nil }
-        guard let data = try? NSJSONSerialization.dataWithJSONObject(si,
+        guard let f = finalize(serialize(nil, object: game)) else { return nil }
+        guard let data = try? NSJSONSerialization.dataWithJSONObject(f,
             options: NSJSONWritingOptions(rawValue: 0)) else {
-            return nil
+                return nil
         }
         return NSString(data: data, encoding: NSUTF8StringEncoding) as? String
     }
     
-    private func serialize(mapables: [String: Mapable]) -> String? {
+    private func finalize(mapped: [Mapped]) -> String? {
+        print("Got Mapped -> \(mapped)")
         fatalError("Not yet implemented")
     }
     
-    private func serialize(object: Mapable, mapped: [String: Mapable]) -> [String: Mapable]? {
+    private func serialize(parent: Mapable?, object: Mapable, var mappeds: [Mapped] = [Mapped]()) -> [Mapped] {
+    
+        var mapped = Mapped()
+        mapped["_class"] = String(object.dynamicType)
+        
+        if let p = parent {
+            mapped["_parent"] = p.map()["uuid"]
+        }
+        
+        object.map().forEach { key, value in
+            if !(value.self is Mapable) {
+                mapped[key] = value
+            }
+        }
         
         let mapables = object.map().filter { key, value in
             return value.self is Mapable
         }
         
+        mappeds.append(mapped)
+        
         for mapable in mapables {
-            return serialize(mapable.1 as! Mapable, mapped: mapped)
+            return serialize(object, object: mapable.1 as! Mapable, mappeds: mappeds)
         }
         
-        return mapped
+        return mappeds
     }
 }
